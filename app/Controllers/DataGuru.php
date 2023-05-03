80a9d537458cfadd8387f319a1288c73b254fd31
anamK4923 <khaerulanam4923.com>
2023-05-03 09:13:08 +0700

    Memperbaiki Data Guru

--- app/Controllers/DataGuru.php
+++ app/Controllers/DataGuru.php
@@ -6,42 +6,42 @@ use App\Models\DataGuruModel;
 
 class DataGuru extends BaseController
 {
-              protected $dataGuruModel;
-              public function __construct()
-              {
-                            $this->dataGuruModel = new DataGuruModel();
-              }
+    protected $dataGuruModel;
+    public function __construct()
+    {
+        $this->dataGuruModel = new DataGuruModel();
+    }
 
-              public function index()
-              {
-                            $data = [
-                                          'title' => 'Data Guru',
-                                          'siswa' => $this->dataGuruModel->getGuru()
-                            ];
+    public function index()
+    {
+        $data = [
+            'title' => 'Data Guru',
+            'guru' => $this->dataGuruModel->getGuru()
+        ];
 
-                            return view('dataGuru/index', $data);
-              }
+        return view('dataGuru/index', $data);
+    }
 
-              public function tambah()
-              {
-                            $data = [
-                                          'title' => 'Tambah Guru'
-                            ];
+    public function tambah()
+    {
+        $data = [
+            'title' => 'Tambah Guru'
+        ];
 
-                            return view('dataGuru/tambah', $data);
-              }
+        return view('dataGuru/tambah', $data);
+    }
 
-              public function simpan()
-              {
-                            $this->dataGuruModel->save([
-                                          'nip'           => $this->request->getVar('nip'),
-                                          'nama_guru'    => $this->request->getVar('nama_guru'),
-                                          // 'tgl_lahir'     => $this->request->getVar('tgl_lahir'),
-                                          'jns_kelamin'   => $this->request->getVar('jns_kelamin'),
-                                          'alamat'        => $this->request->getVar('alamat'),
-                                          'tahun_masuk'   => $this->request->getVar('tahun_masuk')
-                            ]);
+    public function simpan()
+    {
+        $this->dataGuruModel->save([
+            'nip'           => $this->request->getVar('nip'),
+            'nama_guru'    => $this->request->getVar('nama_guru'),
+            // 'tgl_lahir'     => $this->request->getVar('tgl_lahir'),
+            'jns_kelamin'   => $this->request->getVar('jns_kelamin'),
+            'alamat'        => $this->request->getVar('alamat'),
+            'tahun_masuk'   => $this->request->getVar('tahun_masuk')
+        ]);
 
-                            return redirect()->to('/siswa');
-              }
+        return redirect()->to('/siswa');
+    }
 }
